ba86968d91d2c336f6b7547103fcdf31f99b9726
Cheng Fang <cfang@redhat.com>
2023-09-20 15:37:05 -0400

    switch from LGPL to apache 2.0 license

--- src/main/java/org/jboss/ejb3/annotation/TransactionTimeout.java
+++ src/main/java/org/jboss/ejb3/annotation/TransactionTimeout.java
@@ -1,38 +1,34 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2006, Red Hat Middleware LLC, and individual contributors
- * as indicated by the @author tags. See the copyright.txt file in the
- * distribution for a full listing of individual contributors.
+ * Copyright 2023 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.jboss.ejb3.annotation;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-import java.util.concurrent.TimeUnit;
 
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Annotation for specifying the transaction timeout of a newly started
  * transaction when invoking an EJB business method.
- * 
+ *
  * @author <a href="mailto:bill.decoste@jboss.org">William DeCoste</a>
  */
 @Target({METHOD, TYPE})
@@ -43,7 +39,7 @@ public @interface TransactionTimeout {
      * is <code>0</code>, the timeout value is reset to the default
      * value.
      *
-     * @see javax.transaction.TransactionManager#setTransactionTimeout(int) 
+     * @see javax.transaction.TransactionManager#setTransactionTimeout(int)
      */
     long value() default 0;
 
